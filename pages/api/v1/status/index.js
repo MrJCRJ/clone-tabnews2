ffece2e6e80d07c487149ed8d8fddc1a8c8d1ae7
Jose Cicero <mr.jociriju@gmail.com>
2025-08-26 18:19:17 -0300

    ajusta a função de consulta ao banco de dados e melhora o endpoint de status da API com informações detalhadas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "API is working" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+
+  response.status(200).json({
+    update_At: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        maxConnections: parseInt(maxConnections.rows[0].max_connections),
+        currentConnections: currentConnections.rows[0].count
+      }
+    }
+  });
 }
 
 export default status;
--- test/api/v1/status/get.test.js
+++ test/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+  expect(responseBody.update_At).toBeDefined()
+
+  const parsedUpdateAt = new Date(responseBody.update_At).toISOString()
+  expect(responseBody.update_At).toEqual(parsedUpdateAt)
+
+
+  expect(responseBody.dependencies.database.version).toEqual("16.10")
+  expect(responseBody.dependencies.database.maxConnections).toEqual(100)
+  expect(responseBody.dependencies.database.currentConnections).toEqual(1)
 })

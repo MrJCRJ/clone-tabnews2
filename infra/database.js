66e823092e968365da6bbe7fdf8569518675f716
Jose Cicero <mr.jociriju@gmail.com>
2025-08-29 12:04:25 -0300

    refatora a conexão com o banco de dados, adiciona suporte a migrações e melhora os testes de API

--- infra/database.js
+++ infra/database.js
@@ -1,23 +1,10 @@
 import { Client } from "pg"
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+  let client
 
-  })
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
   try {
-    await client.connect()
+    client = await getNewClient()
     const result = await client.query(queryObject)
     return result
   } catch (error) {
@@ -31,4 +18,29 @@ async function query(queryObject) {
 
 export default {
   query,
+  getNewClient,
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,18 @@
 import database from "infra/database";
 
 async function status(request, response) {
+
   const updatedAt = new Date().toISOString();
+
   const postgresVersion = await database.query("SHOW server_version;");
+
   const maxConnections = await database.query("SHOW max_connections;");
+
   const databaseName = process.env.POSTGRES_DB;
   const currentConnections = await database.query({
     text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName]
   });
-
 
   response.status(200).json({
     update_At: updatedAt,
